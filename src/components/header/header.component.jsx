102b36f6ae90eb0cb4a8ebf9c5c8d35e1579af2a
unknown <mietlinski.wojtek@gmail.com>
2020-09-02 23:26:28 +0200

    added media querys to make a webiste mobile friendly

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
-    <Link className='logo-containter' to='/'>
+    <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
